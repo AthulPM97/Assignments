ec615f6cb09666e2defb6944f405be6c58512faa
athulpm <97athulpm@gmail.com>
2022-11-17 20:22:15 +0530

    apply function

--- callApplyBind.js
+++ callApplyBind.js
@@ -4,4 +4,6 @@ var addTothis = function(a,b,c) {
     return this.value + a + b + c;
 };
 
-console.log(addTothis.call(obj, 1,2,3));
+//console.log(addTothis.call(obj, 1,2,3));
+var arr = [1,2,3];
+console.log(addTothis.apply(obj, arr));
